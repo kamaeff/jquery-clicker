d37cf4771c59c00107a06d1e50fa41d36ee93a4b
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-07-03 23:35:53 +0300

    add animation for bg

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,39 @@ console.log(
 );
 
 lucide.createIcons();
-let count = 0;
 
 const btn = $("#btn");
+let userCountStat = $("#count");
 
 // NOTE: Change color and count
+const colors = ["#7DA58D", "60718C", "#BF748E", "#F7C9B5", "#C49766"];
+let count = 0;
+
 btn.on("click", () => {
   btn.toggleClass("active");
 
   count++;
   $("#count").text(count);
+
+  userCountStat.css("color", colors[Math.floor(Math.random() * colors.length)]);
 });
 
 // NOTE: Reset
 $("#reset").on("click", () => {
   count = 0;
 
-  $("#count").text("");
+  userCountStat.text("");
+});
+
+// NOTE: Back animation
+$("#back").on("click", () => {
+  $("body").toggleClass("dark");
+
+  const btnState = $(".btn__state");
+  const currentText = btnState.text();
+  const newText = currentText === "☀️" ? "🌒" : "☀️";
+
+  btnState.text(newText);
+
+  newText === "🌒" ? btnState.css("left", "28px") : btnState.css("left", "2px");
 });
