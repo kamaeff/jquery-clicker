8f1ecae843a54ef2453f40fa4f0720a0ffd0bf4d
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-07-04 00:17:00 +0300

    add prettier

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,41 @@
-import "./type.js";
+import './type.js';
 
-let userCountStat = $("#count");
+let userCountStat = $('#count');
 let count = 0;
 
 // NOTE: Change color and count
 window.clickBtn = () => {
-  const colors = ["#7DA58D", "60718C", "#BF748E", "#F7C9B5", "#C49766"];
-  $(".cliker__btn").toggleClass("active");
+  const colors = ['#7DA58D', '60718C', '#BF748E', '#F7C9B5', '#C49766'];
+  $('.cliker__btn').toggleClass('active');
 
   count++;
-  $("#count").text(count);
+  $('#count').text(count);
 
-  userCountStat.css("color", colors[Math.floor(Math.random() * colors.length)]);
+  userCountStat.css('color', colors[Math.floor(Math.random() * colors.length)]);
 };
 
 // NOTE: Reset
-$("#reset").on("click", () => {
+$('#reset').on('click', () => {
   count = 0;
-  userCountStat.text("0");
+  userCountStat.text('0');
 });
 
 // NOTE: Back animation
 window.changeBg = () => {
-  $("body").toggleClass("dark");
+  $('body').toggleClass('dark');
 
-  const btnState = $(".btn__state");
+  const btnState = $('.btn__state');
   const currentText = btnState.text();
-  const newText = currentText === "☀️" ? "🌒" : "☀️";
+  const newText = currentText === '☀️' ? '🌒' : '☀️';
 
   btnState.text(newText);
 
-  newText === "🌒" ? btnState.css("left", "32px") : btnState.css("left", "2px");
+  newText === '🌒' ? btnState.css('left', '32px') : btnState.css('left', '2px');
 };
 
 $(document).ready(() => {
   console.log(
-    "This is my Jquery clicker project" + "\n" + "Created by Anton Kamaev"
+    'This is my Jquery clicker project' + '\n' + 'Created by Anton Kamaev',
   );
 
   lucide.createIcons();
