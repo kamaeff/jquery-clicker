89d50a3f9a32d8809c328a7a9ddecab5bc8169c8
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-07-04 11:34:08 +0300

    add info and delete reset btn

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,24 @@ import './type.js';
 
 let userCountStat = $('#count');
 let count = 0;
+let stateTap = 1;
+let stateAutoTap = false;
 
 // NOTE: Change color and count
 window.clickBtn = () => {
   const colors = ['#7DA58D', '60718C', '#BF748E', '#F7C9B5', '#C49766'];
   $('.cliker__btn').toggleClass('active');
 
-  count++;
+  count += stateTap;
   $('#count').text(count);
 
   userCountStat.css('color', colors[Math.floor(Math.random() * colors.length)]);
 };
 
-// NOTE: Reset
-$('#reset').on('click', () => {
-  count = 0;
-  userCountStat.text('0');
-});
-
 // NOTE: Back animation
 window.changeBg = () => {
   $('body').toggleClass('dark');
+  $('#clicker').toggleClass('dark--clicker');
 
   const btnState = $('.btn__state');
   const currentText = btnState.text();
@@ -32,6 +29,9 @@ window.changeBg = () => {
 
   newText === '🌒' ? btnState.css('left', '32px') : btnState.css('left', '2px');
 };
+
+$('#byTap').text(`Tap: ${stateTap}`);
+$('#autoTap').text(`AutoTap ${stateAutoTap ? '✅' : '❌'}`);
 
 $(document).ready(() => {
   console.log(
